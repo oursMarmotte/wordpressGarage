a813f6e292ecbc17d88a38a38ba8beebb3c414c0
oursMarmotte <jeangerald.centaure@gmail.com>
2025-11-15 13:25:33 +0100

    test gitignore

--- public/wp-config.php
+++ public/wp-config.php
@@ -23,16 +23,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', '' );
+define( 'DB_NAME', 'genuingmlvoe' );
 
 /** Database username */
-define( 'DB_USER', '' );
+define( 'DB_USER', 'genuingmlvoe' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Leviathan474' );
 
 /** Database hostname */
-define( 'DB_HOST', '' );
+define( 'DB_HOST', 'genuingmlvoe.mysql.db' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
